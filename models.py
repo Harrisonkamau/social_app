2701daa730554055cbea0d28a2081f9a22c2f030
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-04 21:58:46 +0300

    add posts table

--- models.py
+++ models.py
@@ -60,7 +60,7 @@ class Post(Model):
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User], safe=True)
+    DATABASE.create_tables([User, Post], safe=True)
     DATABASE.close()
 
 
